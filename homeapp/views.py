ad23d9043d5347f3119eff3dd329d38495578b89
divinejoshua <divinejsohua>
2021-01-09 13:51:56 +0100

    Location

--- homeapp/views.py
+++ homeapp/views.py
@@ -17,21 +17,25 @@ def home_view(request):
     visitors = forms.visitorForm()
     instance = visitors.save(commit=False)
     instance.ip = ip_address
-    instance.location = getLocation(request, ip_address)
+    instance.location = getLocation(request)
     instance.save() 
-    # print("worked")  
+    print("worked")  
 
     return render(request, "index.html", context)
 
+def getLocation(request):
+    reader = geoip2.database.Reader('geoip/GeoLite2-City.mmdb')
 
+    if request.META.get("REMOTE_ADDR")==False: #This is to get the ip address
+        ip_address = "NONE"
+    else:
+        ip_address = request.META.get("REMOTE_ADDR")
 
-def getLocation(request, ip_address):
-	reader = geoip2.database.Reader('geoip/GeoLite2-City.mmdb')
-	try:
-		reader.city(ip_address) 				# Check if Ip address is a valid one.  "105.112.102.120"
-	except geoip2.errors.AddressNotFoundError as e:
-		return ("INVALID") 
-	else:
-		response = reader.city(ip_address)
-		return response.country.name + ", " + response.city.name
+    try:
+        reader.city(ip_address) 				# Check if Ip address is a valid one.  "105.112.102.120"
+    except geoip2.errors.AddressNotFoundError as e:
+        return ("INVALID") 
+    else:
+        response = reader.city(ip_address)
+        return response.country.name + ", " + response.city.name
 
